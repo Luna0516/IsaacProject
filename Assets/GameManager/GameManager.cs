e7d0b2b1fcc3403e21581432f9b3199921276c3e
Luna0516 <sunysten@gmail.com>
2023-06-07 22:21:44 +0900

    Item - coin
    1 / 2 / 5 / 10 coin

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [Header("#GameManager")]
+    public int coin = 0;
+    public float health = 3;
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
---
+++ Assets/Item/Coin/Scripts/CoinBase.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CoinBase : MonoBehaviour
+{
+    protected int coinCount;
+
+    protected Rigidbody2D rigid;
+
+    protected virtual void OnEnable() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (!(collision.gameObject.CompareTag("Player")))
+            return;
+
+        GameManager.instance.coin += this.coinCount;
+
+        this.rigid.simulated = false;
+
+        Destroy(this.gameObject);
+    }
+}
