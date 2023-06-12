58ddb4178ec895d9d900e2b43a1bebf778889a9a
RaccoonCode <133838321+BurningBaoTree@users.noreply.github.com>
2023-06-12 15:21:03 +0900

    2023-06-12

--- Assets/Script/MonsterScript/bloodycry/bloodycry.cs
+++ Assets/Script/MonsterScript/bloodycry/bloodycry.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bloodycry : MonoBehaviour
+public class bloodycry : EnemyBase
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector3 Headto;
+    GameObject head;
+    GameObject body;
+    SpriteRenderer headsprite;
+    SpriteRenderer bodysprite;
+    Animator headanimator;
+    Animator bodyanimator;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        head = transform.GetChild(1).gameObject;
+        body = transform.GetChild(0).gameObject;
+        headsprite = head.GetComponent<SpriteRenderer>();
+        bodysprite = body.GetComponent<SpriteRenderer>();
+        headanimator = head.GetComponent<Animator>();
+        bodyanimator = body.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+
+
+
+        Movement();
+    }
+    protected override void Movement()
+    {
+        Headto = target.position - transform.position;
+
+        if (Headto.x > 1)
+        {
+            headsprite.flipX = false;
+            bodysprite.flipX = false;
+            bodyanimator.SetInteger("WalkSideway", 1);
+            transform.position += Headto.normalized * speed * Time.deltaTime;
+        }
+        else if (Headto.x < 1)
+        {
+            headsprite.flipX = true;
+            bodysprite.flipX = true;
+            bodyanimator.SetInteger("WalkSideway", 1);
+            transform.position += Headto.normalized * speed * Time.deltaTime;
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+
+        }
     }
 }
