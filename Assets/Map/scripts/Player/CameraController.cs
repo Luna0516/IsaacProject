3217ac0bb8c84ab96df55583f41179cfa4ea6612
Luna0516 <sunysten@gmail.com>
2023-07-10 21:22:52 +0900

    Map
    맵 정보
    Map 폴더 안에 있는 다른 사람들과 겹치는 스크립트는 전부 주석처리

---
+++ Assets/Map/scripts/Player/CameraController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public static CameraController instance;
+
+    public Room currroom;
+
+    public float moveSpeedWhenRoomChange;
+
+    void Awake()
+    {
+        instance = this;
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    void UpdatePosition()
+    {
+        if(currroom == null)
+        {
+            return;
+        }
+
+        Vector3 targetPos = GetCameraTargetPosition();
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeedWhenRoomChange);
+    }
+
+    Vector3 GetCameraTargetPosition()
+    {
+        if (currroom == null)
+        {
+            return Vector3.zero;
+        }
+
+        Vector3 targetPos = currroom.GetRoomCentre();
+        targetPos.z = transform.position.z;
+
+        return targetPos;
+    }
+
+    public bool IsSwitchingScene()
+    {
+        return transform.position.Equals(GetCameraTargetPosition()) == false;
+    }
+}
