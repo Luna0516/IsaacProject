740c59e37bcc474ef90c3630059aab7e07db581c
Luna0516 <sunysten@gmail.com>
2023-07-11 20:47:06 +0900

    Item
    Active 아이템 먹으면 좌측 상단에 표시

--- Assets/UI/Scripts/UI/ActiveInventory.cs
+++ Assets/UI/Scripts/UI/ActiveInventory.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActiveInventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    int iconLength = 4;
+
+    ActiveItem activeItem;
+    Image icon;
+    ItemBase[] items = new ItemBase[4];
+
+    private void Awake() {
+        icon = transform.GetChild(0).GetComponent<Image>();
+
+        FindItem();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void FindItem() {
+        items = FindObjectsOfType<ItemBase>();
         
+        if(items != null) {
+            foreach(ItemBase one in items) {
+                if(one.activeItem == null) {
+                    continue;
+                }
+                one.setItem += SetActive;
+            }
+        }
+    }
+
+    void SetActive(ActiveItem item) {        
+        icon.sprite = item.Icon;
+        icon.color = Color.white;
+
+        if(item.CoolTime == 4) {
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
     }
 }
