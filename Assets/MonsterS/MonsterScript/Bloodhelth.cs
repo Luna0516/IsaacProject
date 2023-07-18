2bce9eefa09b9dc3d0d993c6f79ffd9788b154f6
RaccoonCode <133838321+BurningBaoTree@users.noreply.github.com>
2023-07-18 15:21:30 +0900

    2023-07-18
    피는 해결했음, 이젠 고깃덩이 해결해야함

--- Assets/MonsterS/MonsterScript/Bloodhelth.cs
+++ Assets/MonsterS/MonsterScript/Bloodhelth.cs
@@ -6,27 +6,27 @@ public class Bloodhelth : MonoBehaviour
 {
     Color clo;
     SpriteRenderer spritealpha;
-    float lifeTime;
+    public float lifeTime;
     float thislifeTime;
 
-    private void Awake()
-    {
-        spritealpha = GetComponent<SpriteRenderer>();
-    }
     private void Start()
     {
-        lifeTime = Random.Range(8f, 15f);
+        spritealpha = GetComponent<SpriteRenderer>();
+        lifeTime = Random.Range(15f, 50f);
         clo = spritealpha.color;
         thislifeTime = lifeTime;
     }
+
     private void Update()
     {
-        clo.a = Mathf.Lerp(1f,0f, lifeTime/thislifeTime);
         lifeTime -= Time.deltaTime;
-        if (lifeTime < 0 )
+        float guage = Mathf.Lerp(0f, 1f, lifeTime / thislifeTime);
+        clo.a = guage;
+        spritealpha.color = clo;
+
+        if (lifeTime < 0)
         {
-            Destroy( this );
+            Destroy(gameObject);
         }
     }
-
 }
---
+++ Assets/MonsterS/MonsterScript/Meatshit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meatshit : MonoBehaviour
+{
+
+   //고깃덩이가 이동하는 코드를 짜야한다.
+    Vector2 moveDir;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+       // transform.Translate();
+    }
+}
