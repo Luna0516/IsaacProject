23d374d0c073c9135d4a594219e861573fdd9d4a
Luna0516 <sunysten@gmail.com>
2023-07-24 22:00:37 +0900

    Item, UI
    아이템 생성 바위 프리펩화(미구현)
    UI 시작 장면 , Attribute 스크립트 수정, 게임매니저 주석

--- Assets/UI/Scripts/UI/GameManager.cs
+++ Assets/UI/Scripts/UI/GameManager.cs
@@ -5,49 +5,68 @@ using System;
 
 public class GameManager : Singleton<GameManager>
 {
+    /// <summary>
+    /// 플레이어
+    /// </summary>
     Player player;
 
-
+    /// <summary>
+    /// 플레이어 프로퍼티
+    /// </summary>
     public Player Player
     {
         get 
         { 
             if (player == null)
             {
-                OnInitialize();
+                player = FindObjectOfType<Player>();
             }
             return player; 
         }
     }
 
+    /// <summary>
+    /// 액티브인벤토리
+    /// </summary>
     ActiveInventory activeInventory;
+
+    /// <summary>
+    /// 액티브인벤토리 프로퍼티
+    /// </summary>
     public ActiveInventory ActiveInventory
     {
         get 
         {
             if (activeInventory == null) 
             {
-                OnInitialize();
+                activeInventory = FindObjectOfType<ActiveInventory>();
             }
             return activeInventory;
         }
     }
 
-    protected override void OnInitialize()
-    {
-        base.OnInitialize();
-        player = FindObjectOfType<Player>();
-        activeInventory = FindObjectOfType<ActiveInventory>();
-    }
-
+    /// <summary>
+    /// 아이템로드 액션 델리게이트
+    /// </summary>
     public Action LoadItem;
 
+    protected override void OnInitialize()
+    {
+        base.OnInitialize();   
+    }
+
+    /// <summary>
+    /// 일시정지
+    /// </summary>
     public void PauseGame() 
     {
         Time.timeScale = 0;
         Time.fixedDeltaTime = 0;
     }
 
+    /// <summary>
+    /// 일시정지 해제
+    /// </summary>
     public void QuitPauseGame() 
     {
         Time.timeScale = 1;
