074091d5be842d39ef894a4f98b7a135c42b6f68
BurningBaoTree <133838321+BurningBaoTree@users.noreply.github.com>
2023-08-02 21:19:07 +0900

    똥/파리 코드 수정
    똥 코드 내부에서 코루틴이 코루틴을 실행하는 코드 수정, 파리 움직임 기믹을 추가해보았습니다.

--- Assets/MonsterS/MonsterScript/Fly.cs
+++ Assets/MonsterS/MonsterScript/Fly.cs
@@ -4,55 +4,88 @@ using UnityEngine;
 
 public class Fly : EnemyBase
 {
-    Vector3 headto;
-    float invincivalTime = 2f;
-    bool invincival=true;
-    Animator animator;
+	Vector3 headto;
+	Vector2 Rnad;
+	float invincivalTime = 1f;
+	bool invincival = true;
+	Animator animator;
+	SpriteRenderer rneder;
+	Collider2D coll;
 
-    float InvincivalTime
-    { 
-        get 
-        { 
-            return invincivalTime; 
-        } 
-        set 
-        {
-            if(invincivalTime != value)
-            {
-            invincivalTime = value;
-            if (invincivalTime < 0)
-            {
-            invincival = false;
-            }
-            }
-        }
-    }
+	float InvincivalTime
+	{
+		get
+		{
+			return invincivalTime;
+		}
+		set
+		{
+			if (invincivalTime != value)
+			{
+				invincivalTime = value;
+				if (invincivalTime < 0)
+				{
+					invincival = false;
+				}
+			}
+		}
+	}
 
-    private void Start()
-    {
-        animator = GetComponent<Animator>();    
-        speed = Random.Range(0.5f, 1.5f);
-    }
-    private void Update()
-    {
-        InvincivalTime -= Time.deltaTime;
-        headto = (target.position - transform.position).normalized;
-        this.gameObject.transform.Translate(Time.deltaTime*speed* headto);
-    }
-    protected override void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(!invincival)
-        {
-        if (collision.gameObject.CompareTag("PlayerBullet"))
-        {
-            damage = collision.gameObject.GetComponent<AttackBase>().Damage;
-            Hitten();
-        }
-        }
-    }
-    protected override void Die()
-    {
-        animator.SetInteger("Dead", 1);
-        Destroy(this.gameObject,0.917f);
-    }
+	private void Start()
+	{
+		coll = GetComponent<Collider2D>();
+		rneder = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
+		speed = Random.Range(0.5f, 2f);
+		StartCoroutine(IvincivalFly());
+	}
+	void noisyMove()
+	{
+		if (coll.isTrigger==false)
+		{
+			float X = Random.Range(-10, 10.1f);
+			float Y = Random.Range(-10, 10.1f);
+			Rnad = new Vector2(X, Y);
+			this.gameObject.transform.Translate(Time.deltaTime * speed * Rnad);
+		}
+	}
+	private void Update()
+	{
+		headto = (target.position - transform.position).normalized;
+		if (headto.x > 0)
+		{
+			rneder.flipX = true;
+		}
+		else
+		{
+			rneder.flipX = false;
+		}
+		this.gameObject.transform.Translate(Time.deltaTime * speed * headto);
+		noisyMove();
+	}
+	protected override void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (!invincival)
+		{
+			if (collision.gameObject.CompareTag("PlayerBullet"))
+			{
+				damage = collision.gameObject.GetComponent<AttackBase>().Damage;
+				Hitten();
+			}
+		}
+	}
+	protected override void Die()
+	{
+		coll.isTrigger = true;
+		animator.SetInteger("Dead", 1);
+		Destroy(this.gameObject, 0.917f);
+	}
+	IEnumerator IvincivalFly()
+	{
+		while (invincival)
+		{
+			InvincivalTime -= Time.deltaTime;
+			yield return null;
+		}
+	}
 }
--- Assets/MonsterS/MonsterScript/shit.cs
+++ Assets/MonsterS/MonsterScript/shit.cs
@@ -6,158 +6,182 @@ using static PlayerAction;
 
 public class shit : EnemyBase
 {
-    Rigidbody2D rig;
-    Animator animator;
-    SpriteRenderer spriteRenderer;
-    Vector3 Headto;
-    public Color bloodColor = Color.white;
-    public float power = 1f;
-    public float coolTime = 5;
-    float coolTimeCopy;
-    public GameObject flyer;
-    bool Attacking = false;
-    WaitForSeconds waiting;
-    float NextStateTime = 1;
-    bool shitDead=false;
-    public bool ShitDead
-    {
-        get => shitDead;
-        set
-        {
-            shitDead = value;
-        }
-    }
-    public System.Action<bool> IsDead;
-    protected override void Awake()
-    {
-        base.Awake();
-        rig = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        waiting = new WaitForSeconds(NextStateTime);
-    }
-    private void Start()
-    {
-        StartCoroutine(attackcool());
-    }
-    private void Update()
-    {
-        Headto = (target.position - transform.position).normalized;
-        if (Headto.x < 0)
-        {
-            spriteRenderer.flipX = true;
-        }
-        else
-        { spriteRenderer.flipX = false; }
-    }
+	Rigidbody2D rig;
+	Animator animator;
+	SpriteRenderer spriteRenderer;
+	Vector3 Headto;
+	public Color bloodColor = Color.white;
+	public float power = 1f;
+	public float coolTime = 5;
+	public GameObject flyer;
 
-    protected override void OnEnable()
-    {
-        base.OnEnable();
-        coolTimeCopy = coolTime;
-    }
+	WaitForSeconds waiting;
+	float NextStateTime = 1;
+	bool shitDead = false;
+	bool Attacking = false;
 
 
-    void Attack()
-    {
-        rig.isKinematic = false;
-        Attacking = true;
-        rig.AddForce(Headto * power, ForceMode2D.Impulse);
-    }
-    private void Stopping()
-    {
-        Attacking = false;
-        rig.isKinematic = true;
-        rig.velocity = Vector2.zero;
-    }
-    IEnumerator attackcool()
-    {
-        while (true)
-        {
-            if (coolTime > 0)
-            {
-                coolTime -= Time.deltaTime;
-                yield return null;
-            }
-            else
-            {
-                animator.SetTrigger("Attack");
-                yield return null;
-                Attack();
-                StartCoroutine(runningShit());
-                coolTime = coolTimeCopy;
-                NextStateTime = 0.917f;
-                yield return waiting;
-                Stopping();
-                NextStateTime = coolTime;
-                yield return waiting;
-                coolTime = Random.Range(3, 5);
-            }
-        }
-    }
-    IEnumerator runningShit()
-    {
-        while (Attacking)
-        {
-            GameObject shitiything = Factory.Inst.GetObject(PoolObjectType.EnemyShit, transform.position);
-            Shitblood bloodobj = shitiything.GetComponent<Shitblood>();
-            IsDead += bloodobj.EnamvleChoosAction;
-            IsDead?.Invoke(false);
-            IsDead -= bloodobj.EnamvleChoosAction;
-            yield return new WaitForSeconds(0.05f);
+	public bool ShitDead
+	{
+		get => shitDead;
+		set
+		{
+			shitDead = value;
+		}
+	}
+	bool AttacActive
+	{
+		get => Attacking;
+		set
+		{
+			Attacking = value;
+			IsAttack?.Invoke(this);
+		}
+	}
+	System.Action<bool> IsDead;
+	System.Action<bool> IsAttack;
+	protected override void Awake()
+	{
+		base.Awake();
+		rig = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		waiting = new WaitForSeconds(NextStateTime);
+	}
+	private void Start()
+	{
+		coolTime = Random.Range(3, 5);
+		StartCoroutine(CoolTiming());
+	}
+	private void Update()
+	{
+		Headto = (target.position - transform.position).normalized;
+		if (Headto.x < 0)
+		{
+			spriteRenderer.flipX = true;
+		}
+		else
+		{ spriteRenderer.flipX = false; }
+	}
 
-        }
-    }
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+		IsAttack += Attack;
+	}
+	private void OnDestroy()
+	{
+		StopAllCoroutines();
+		IsAttack -= Attack;
+	}
 
-    protected override void Die()
-    {
-        bloodshatter();
-        int sellect = 0;
-        int rand = UnityEngine.Random.Range(0, 101);
-        if (rand < 40)
-        {
-            sellect = 0;
-        }
-        else if (rand < 60)
-        {
-            sellect = 3;
-        }
-        else if (rand < 80)
-        {
-            sellect = 4;
-        }
-        else if (rand < 101)
-        {
-            sellect = 5;
-        }
+	void Attack(bool Attackchack)
+	{
+		coolTime = Random.Range(3, 5);
+		rig.isKinematic = false;
+		rig.AddForce(Headto * power, ForceMode2D.Impulse);
+		if (Attackchack)
+		{
+			StartCoroutine(attackAction());
+			StartCoroutine(runningShit());
+		}
+	}
+	private void Stopping()
+	{
+		AttacActive = false;
+		rig.isKinematic = true;
+		rig.velocity = Vector2.zero;
+		StartCoroutine(CoolTiming());
+	}
+	IEnumerator CoolTiming()
+	{
+		while (true)
+		{
+			if (coolTime > 0)
+			{
+				coolTime -= Time.deltaTime;
+				yield return null;
+			}
+			else
+			{
+				AttacActive = true;
+				yield break;
+			}
+		}
+	}
+	IEnumerator attackAction()
+	{
+		while (AttacActive)
+		{
+			animator.SetTrigger("Attack");
+			yield return null;
+			NextStateTime = 0.917f;
+			yield return waiting;
+			Stopping();
+		}
+	}
+	IEnumerator runningShit()
+	{
+		while (AttacActive)
+		{
+			GameObject shitiything = Factory.Inst.GetObject(PoolObjectType.EnemyShit, transform.position);
+			Shitblood bloodobj = shitiything.GetComponent<Shitblood>();
+			IsDead += bloodobj.EnamvleChoosAction;
+			IsDead?.Invoke(false);
+			IsDead -= bloodobj.EnamvleChoosAction;
+			yield return new WaitForSeconds(0.05f);
+		}
+	}
 
-        float ranX = Random.Range(-1, 1.1f);
-        float ranY = Random.Range(-1, 1.1f);
+	protected override void Die()
+	{
+		bloodshatter();
+		int sellect = 0;
+		int rand = UnityEngine.Random.Range(0, 101);
+		if (rand < 40)
+		{
+			sellect = 0;
+		}
+		else if (rand < 60)
+		{
+			sellect = 3;
+		}
+		else if (rand < 80)
+		{
+			sellect = 4;
+		}
+		else if (rand < 101)
+		{
+			sellect = 5;
+		}
 
-        Vector2 pos = new Vector2(ranX, ranY);
-        for (int i = 0; i < sellect; i++)
-        {
-            GameObject fly = Instantiate(flyer, (Vector2)this.transform.position + pos, this.transform.rotation);
-        }
-        Destroy(this.gameObject);//피를 다 만들고 나면 이 게임 오브젝트는 죽는다.
-    }
+		float ranX = Random.Range(-1, 1.1f);
+		float ranY = Random.Range(-1, 1.1f);
 
-    protected override void bloodshatter()
-    {
-        for (int i = 0; i < 2; i++)//피의 갯수만큼 반복작업
-        {
-            float X = Random.Range(transform.position.x - 0.5f, transform.position.x + 0.5f);//피의 위치 조절용 X축
-            float Y = Random.Range(transform.position.y - 0.3f, transform.position.y);//피의 위치 조절용 Y축
-            GameObject bloodshit = Factory.Inst.GetObject(PoolObjectType.EnemyShit, new Vector2(X, Y));
-            Shitblood bloodobj = bloodshit.GetComponent<Shitblood>();
-            IsDead += bloodobj.EnamvleChoosAction;
-            IsDead?.Invoke(true);
-        }
-    }
-    protected override void Hitten()
-    {
-        base.Hitten();
-        StartCoroutine(damaged(spriteRenderer));
-    }
+		Vector2 pos = new Vector2(ranX, ranY);
+		for (int i = 0; i < sellect; i++)
+		{
+			GameObject fly = Instantiate(flyer, (Vector2)this.transform.position + pos, this.transform.rotation);
+		}
+		Destroy(this.gameObject);//피를 다 만들고 나면 이 게임 오브젝트는 죽는다.
+	}
+
+	protected override void bloodshatter()
+	{
+		for (int i = 0; i < 2; i++)//피의 갯수만큼 반복작업
+		{
+			float X = Random.Range(transform.position.x - 0.5f, transform.position.x + 0.5f);//피의 위치 조절용 X축
+			float Y = Random.Range(transform.position.y - 0.3f, transform.position.y);//피의 위치 조절용 Y축
+			GameObject bloodshit = Factory.Inst.GetObject(PoolObjectType.EnemyShit, new Vector2(X, Y));
+			Shitblood bloodobj = bloodshit.GetComponent<Shitblood>();
+			IsDead += bloodobj.EnamvleChoosAction;
+			IsDead?.Invoke(true);
+		}
+	}
+	protected override void Hitten()
+	{
+		base.Hitten();
+		StartCoroutine(damaged(spriteRenderer));
+	}
 
 }
