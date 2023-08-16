191f3f78e26252f221f9668ae35f0f577de7f916
Luna0516 <sunysten@gmail.com>
2023-08-16 20:58:37 +0900

    UI - PauseManu
    애니메이션 써서 끝냄
    버튼 오류 난건 이유 모름 => 캔버스 새로 만들어서 하니까 됨
    뭘까...
    버튼 눌렀을 때 실행할 함수는 아직 안만듬

--- Assets/UI/Scripts/Pause.cs
+++ Assets/UI/Scripts/Pause.cs
@@ -1,48 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    /// <summary>
+    /// 메뉴가 열려있는지 확인하기 위한 bool값
+    /// </summary>
+    bool isOpen = false;
+
     Animator anim;
 
-    // 퍼지 할때 할꺼
-    Button newRun;
-    Button resumeGame;
-    Button exitGame;
-
-    readonly int Hash_IsPause = Animator.StringToHash("IsPause");
+    UIInputAction UiInput;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
 
+        UiInput = new UIInputAction();
+
         Transform child = transform.GetChild(0);
-        newRun = child.GetComponent<Button>();
+        Button newRun = child.GetComponent<Button>();
         newRun.onClick.AddListener(NewRun);
 
         child = transform.GetChild(1);
-        resumeGame = child.GetComponent<Button>();
-        resumeGame.onClick.AddListener(ResumGame);
+        Button resumeGame = child.GetComponent<Button>();
+        resumeGame.onClick.AddListener(ResumeGame);
 
         child = transform.GetChild(2);
-        exitGame = child.GetComponent<Button>(); 
+        Button exitGame = child.GetComponent<Button>();
         exitGame.onClick.AddListener(ExitGame);
     }
 
-    void NewRun()
+    private void OnEnable()
+    {
+        UiInput.UI.Enable();
+        UiInput.UI.Pause.performed += OnPause;
+    }
+
+    private void OnDisable()
+    {
+        UiInput.UI.Pause.performed -= OnPause;
+        UiInput.UI.Disable();
+    }
+
+    private void OnPause(InputAction.CallbackContext _)
+    {
+        StartCoroutine(PauseDelay(isOpen));
+    }
+
+    /// <summary>
+    /// Esc버튼을 눌렀을 때 실행할 코루틴
+    /// </summary>
+    /// <param name="active">메뉴가 열려있는지 확인하는 bool값</param>
+    IEnumerator PauseDelay(bool active)
+    {
+        if (active)
+        {
+            anim.SetBool("Open", false);
+            yield return new WaitForSeconds(0.5f);
+            isOpen = !active;
+        }
+        else
+        {
+            anim.SetBool("Open", true);
+            yield return new WaitForSeconds(0.5f);
+            isOpen = !active;
+        }
+    }
+
+    /// <summary>
+    /// NewRun을 눌렀을 때 실행할 함수
+    /// </summary>
+    private void NewRun()
     {
         Debug.Log("NewRun");
     }
 
-    void ResumGame()
+    /// <summary>
+    /// ResumeGame 눌렀을 때 실행할 함수
+    /// </summary>
+    private void ResumeGame()
     {
-        Debug.Log("ResumGame");
+        Debug.Log("ResumeGame");
     }
 
-    void ExitGame()
+    /// <summary>
+    /// ExitGame 눌렀을 때 실행할 함수
+    /// </summary>
+    private void ExitGame()
     {
         Debug.Log("ExitGame");
     }
