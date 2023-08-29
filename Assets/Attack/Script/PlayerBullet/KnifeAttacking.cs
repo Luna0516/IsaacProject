3d995409d83447239594f2cca518e3e2a917141b
SengbumKim <133838075+SengbumKim@users.noreply.github.com>
2023-08-29 15:20:47 +0900

    8/29 KnifeAttacking 수정 중
    - 여전히 공격 위치에서 돌아오지 않음. (수정 필요)

--- Assets/Attack/Script/PlayerBullet/KnifeAttacking.cs
+++ Assets/Attack/Script/PlayerBullet/KnifeAttacking.cs
@@ -3,51 +3,48 @@ using UnityEngine;
 
 public class KnifeAttacking : AttackBase
 {
-    private Vector2 startPos;    // 투사체 발사 시 초기 위치
-    private Vector2 targetPos;   // 투사체 목표 위치
+    private Vector2 startPos;
+    private Vector2 targetPos;
+    private bool isReturning = false;
 
-    bool isMoving = true;
-
-    protected override void Awake()
-    {
-        base.Awake();
-    }
-
-    private void Start()
-    {
-        
-    }
     protected override void Init()
     {
         base.Init();
 
-        startPos = transform.position;
-        targetPos = startPos * dir * player.Range;
-        
-        dir = (targetPos - startPos).normalized;
+        if(gameObject.CompareTag("Player"))
+        {
+            startPos = transform.position;
+        }
+       
+        targetPos = startPos + dir * player.Range;
+
+        // 초기화 시 발사 방향과 반대 방향으로 설정
+        dir = -dir * startPos;
     }
 
     protected override void OnEnable()
     {
-        //base.OnEnable();       
+       
     }
 
     protected override void FixedUpdate()
     {
-        Vector2 targetEnd = isMoving ? targetPos : startPos;
+        Vector2 targetEnd = isReturning ? startPos : targetPos;
 
         float distanceToTarget = Vector3.Distance(transform.position, targetEnd);
 
-        if (distanceToTarget > 0.1f) // 움직일 거리가 남았을 때만 이동
+        if (distanceToTarget > 0.1f)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetEnd, speed * Time.deltaTime);
         }
         else
         {
-            transform.position = -dir * Time.deltaTime; // 이동할 거리가 없으면 방향을 바꾸어 줍니다.
+           
+            isReturning = true;
+            targetPos = startPos;
+            
         }
     }
-
 
     protected override IEnumerator Gravity_Life(float delay = 0)
     {
