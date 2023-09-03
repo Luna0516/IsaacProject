b68b0bb9af220576fc4fab50ad26b9fcede9492d
Luna0516 <sunysten@gmail.com>
2023-09-03 19:31:38 +0900

    Item
    상자 열릴때 아이템 퍼지게 만듬

--- Assets/Items/Scripts/GoldenChest.cs
+++ Assets/Items/Scripts/GoldenChest.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class GoldenChest : Chest
 {
+    /// <summary>
+    /// 아이템 종류와 드랍 확률
+    /// </summary>
+    [System.Serializable]
+    public struct DropPassiveItem
+    {
+        public PassiveItem passiveItemCode;
+
+        [Range(0.0f, 1.0f)]
+        public float dropPercentage;
+    }
+
+    /// <summary>
+    /// 드랍 아이템 구조체 배열
+    /// </summary>
+    public DropPassiveItem[] dropPassiveItem;
+
     protected override void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
             Player player = collision.gameObject.GetComponent<Player>();
+
             if (!IsOpen && player.Key > 0) {    // 플레이어의 열쇠 개수가 1개 이상이고 열려있지 않다면 실행
                 player.Key--;
                 IsOpen = true;
@@ -19,16 +37,25 @@ public class GoldenChest : Chest
     /// <summary>
     /// 상자가 열렸을 때 아이템 생성 함수
     /// </summary>
-    void ChestOpen() {
-        foreach (var item in dropItems) {
-            if (Random.value < item.dropPercentage) {
-                //GameObject itemObj = ItemFactory.Inst.CreateItem(item.data, new Vector2(transform.position.x + Random.Range(-1.0f, 1.0f), transform.position.y + Random.Range(-1.0f, 1.0f)));
-                //Rigidbody2D itemRigid = itemObj.GetComponent<Rigidbody2D>();
-                //if (itemRigid != null)
-                //{
-                //    Vector2 dir = (itemObj.transform.position - transform.position).normalized;
-                //    itemRigid.AddForce(dir * 25.0f, ForceMode2D.Impulse);
-                //}
+    protected override void ChestOpen() {
+        base.ChestOpen();
+
+        foreach (var item in dropPassiveItem)
+        {
+            if (Random.value < item.dropPercentage)
+            {
+                Vector3 spawnPos = Vector2.one * Random.Range(-0.2f, 0.2f);
+
+                GameObject itemObj = ItemFactory.Inst.CreatePassiveItem(item.passiveItemCode, transform.position + spawnPos);
+
+                Rigidbody2D targetRigid = itemObj.gameObject.GetComponent<Rigidbody2D>();
+
+                if (targetRigid != null)
+                {
+                    Vector3 force = (itemObj.transform.position - transform.position).normalized;
+
+                    targetRigid.AddForce(force, ForceMode2D.Impulse);
+                }
             }
         }
     }
