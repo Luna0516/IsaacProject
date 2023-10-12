59871dc0324e352d36c1acbab77df5078832f289
RaccoonCode <133838321+BurningBaoTree@users.noreply.github.com>
2023-10-12 14:21:37 +0900

    브림스톤 이펙트 완성

--- Assets/Attack/Script/PlayerBullet/BrimStone.cs
+++ Assets/Attack/Script/PlayerBullet/BrimStone.cs
@@ -3,23 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
-
-
 public class BrimStone : PooledObject
 {
 
+    SpriteRenderer spriteRenderer;
+
     Action signal;
 
+    float actionTimer;
+
+    Vector2 spritesize;
+    Vector2 defaltsize;
     /// <summary>
     /// 플레이어 (아이작)
     /// </summary>
     Player player;
-
-    /// <summary>
-    /// 총알 발사 위치
-    /// </summary>
-    Transform firePos;
 
     /// <summary>
     /// 현재 방의 위치를 알기 위한 변수
@@ -27,23 +25,9 @@ public class BrimStone : PooledObject
     Room room;
 
     /// <summary>
-    /// 시작 점 X
-    /// </summary>
-    float startDotX;
-    /// <summary>
-    /// 시작 점 Y
-    /// </summary>
-    float startDotY;
-
-    /// <summary>
     /// 순서대로 위쪽 벽의 y, 아래 벽의 y, 왼쪽 벽의 x, 오른쪽 벽의 x
     /// </summary>
     float[] roomDistance = new float[4];
-
-    /// <summary>
-    /// 총 늘어날 거리
-    /// </summary>
-    float totalDistance;
 
     /// <summary>
     /// 발사 방향
@@ -51,20 +35,21 @@ public class BrimStone : PooledObject
     int dir;
 
     bool isPressed = false;
-
+    bool shotAble = false;
+    bool isFireing = false;
     // 계수 관련 변수---------------------------------------------- 
 
     float speed;
     float damage;
 
     float chargeGage = 0f;
-    float maxGage = 5f;
+    float maxGage = 1f;
     // ------------------------------------------------------------
 
 
 
     float ChargeGage
-    { 
+    {
         get
         {
             return chargeGage;
@@ -73,94 +58,53 @@ public class BrimStone : PooledObject
         set
         {
             chargeGage = value;
-            if(chargeGage > maxGage)
+            if (chargeGage > maxGage)
             {
                 chargeGage = maxGage;
+                shotAble = true;
             }
         }
     }
 
-
+    private void Awake()
+    {
+        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        defaltsize = new Vector2(2.95f, 0);
+        spritesize = defaltsize;
+        signal = () => { };
+    }
     private void OnEnable()
     {
-        transform.localScale = Vector3.zero;
-
+        spriteRenderer.size = spritesize;
         room = RoomManager.Inst.CurrentRoom;
-
-
-
-        if (player.AttackDir.y >= 1.0f && player.AttackDir.x == 0)
-        {
-            dir = 0;
-        }
-        else if (player.AttackDir.y <= -1.0f && player.AttackDir.x == 0)
-        {
-            dir = 1;
-        }
-        else if (player.AttackDir.x <= -1.0f && player.AttackDir.y == 0)
-        {
-            dir = 2;
-        }
-        else if (player.AttackDir.x >= 1.0f && player.AttackDir.y == 0)
-        {
-            dir = 3;
-        }
-
-        if (dir >= 2)
-        {
-            transform.rotation = Quaternion.Euler(Vector3.forward * -90);
-        }
     }
 
     private void Start()
     {
         player = GameManager.Inst.Player;
-        firePos = player.gameObject.transform.GetChild(0);
 
-        if(player != null)
+        if (player != null)
         {
             Init();
         }
-
+        //위
         roomDistance[0] = room.MyPos.y * 10 - (10 * 0.5f - 1);
+        //아래
         roomDistance[1] = room.MyPos.y * 10 + (10 * 0.5f - 1);
+        //왼
         roomDistance[2] = room.MyPos.x * 17.9f - (17.9f * 0.5f - 1);
+        //오
         roomDistance[3] = room.MyPos.x * 17.9f + (17.9f * 0.5f - 1);
     }
 
     private void FixedUpdate()
     {
         signal();
-
-        if (player.AttackDir != Vector2.zero)
-        {
-            transform.position = firePos.position;
-
-            startDotX = firePos.position.y;
-            startDotY = firePos.position.x;
-
-            if (dir >= 2)
-            {
-                totalDistance = startDotX - roomDistance[dir];
-            }
-            else
-            {
-                totalDistance = startDotY - roomDistance[dir];
-            }
-
-            transform.localScale = new Vector3(1, totalDistance * 0.25f, 1);
-        }
-        else
-        {
-            StartCoroutine(Gravity_Life(2.0f));
-        }
     }
-   public void Press()
+    public void Press()
     {
-        if(!isPressed)
+        if (!isPressed && !isFireing)
         {
-            Debug.Log("눌렀다.");
-            gameObject.SetActive(false);
             isPressed = true;
             signal += Charging;
         }
@@ -170,17 +114,23 @@ public class BrimStone : PooledObject
     {
         if (isPressed)
         {
-            Debug.Log("놓았다.");
-            gameObject.SetActive(true);
             ChargeGage = 0;
             signal -= Charging;
+            if (shotAble)
+            {
+                actionTimer = 0.7f;
+                isFireing = true;
+                signal += FireActive;
+            }
             isPressed = false;
+            shotAble = false;
         }
     }
 
     public void Charging()
     {
-        chargeGage += Time.deltaTime * speed; 
+        ChargeGage += Time.deltaTime * speed;
+        fireDirCal(player.AttackDir);
     }
 
     private void Init()
@@ -189,6 +139,72 @@ public class BrimStone : PooledObject
         {
             speed = player.TearSpeed;
             damage = player.Damage;
+        }
+    }
+
+    void FireActive()
+    {
+        if (actionTimer > 0)
+        {
+            wallCal();
+            spriteRenderer.size = spritesize;
+            actionTimer -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            isFireing = false;
+            spritesize = defaltsize;
+            spriteRenderer.size = spritesize;
+            signal -= FireActive;
+        }
+    }
+
+    void fireDirCal(Vector2 dircal)
+    {
+        if (dircal.y >= 1.0f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+            dir = 0;
+        }
+        else if (dircal.y <= -1.0f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            dir = 1;
+        }
+        else if (dircal.x <= -1.0f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+            dir = 2;
+        }
+        else if (dircal.x >= 1.0f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+            dir = 3;
+        }
+    }
+    void wallCal()
+    {
+        float legth;
+        switch (dir)
+        {
+            case 0:
+                legth = roomDistance[1] - player.transform.position.y;
+                spritesize.y = legth;
+                break;
+            case 1:
+                legth = player.transform.position.y - roomDistance[0];
+                spritesize.y = legth+0.5f;
+                break;
+            case 2:
+                legth = player.transform.position.x - roomDistance[2];
+                spritesize.y = legth;
+                break;
+            case 3:
+                legth = roomDistance[3] - player.transform.position.x;
+                spritesize.y = legth;
+                break;
+            default:
+                break;
         }
     }
 
