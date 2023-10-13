c37f446e0ecceafa3a7da248adffef996b6da958
SengbumKim <133838075+SengbumKim@users.noreply.github.com>
2023-10-13 19:27:51 +0900

    _

--- Assets/Map/scripts/Room.cs
+++ Assets/Map/scripts/Room.cs
@@ -278,7 +278,7 @@ public class Room : MonoBehaviour
             if (roomtype == RoomType.Start)
             {
                 /*GameObject itemObj = ItemFactory.Inst.CreatePassiveItem((PassiveItem)(Random.Range(0, System.Enum.GetValues(typeof(PassiveItem)).Length)));*/
-                GameObject itemObj = ItemFactory.Inst.CreatePassiveItem(PassiveItem.Brimstone);
+                GameObject itemObj = ItemFactory.Inst.CreatePassiveItem(PassiveItem.MomsKnife);
                 itemObj.transform.position = itemSpawnPos;
                 return;
             }
